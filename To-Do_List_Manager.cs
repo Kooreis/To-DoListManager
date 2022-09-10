13f59123f1a2014a717c2e691ce6e70f429cc25d
Kooreis <kooreis.official@gmail.com>
2022-09-10 06:00:00 -0500

    In this update, the necessary libraries are imported and the main class and method are defined. A list to store the to-do tasks and a string to store the user's command are also initialized.

--- To-Do_List_Manager.cs
+++ To-Do_List_Manager.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 using System.Collections.Generic;
 
@@ -8,48 +7,5 @@ class Program
     {
         List<string> toDoList = new List<string>();
         string command = "";
-
-        while (command != "exit")
-        {
-            Console.WriteLine("Enter command (add, remove, view, exit): ");
-            command = Console.ReadLine();
-
-            switch (command)
-            {
-                case "add":
-                    Console.WriteLine("Enter task: ");
-                    string task = Console.ReadLine();
-                    toDoList.Add(task);
-                    break;
-
-                case "remove":
-                    Console.WriteLine("Enter task number to remove: ");
-                    int taskNumber = Convert.ToInt32(Console.ReadLine());
-                    if (taskNumber > 0 && taskNumber <= toDoList.Count)
-                    {
-                        toDoList.RemoveAt(taskNumber - 1);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid task number.");
-                    }
-                    break;
-
-                case "view":
-                    for (int i = 0; i < toDoList.Count; i++)
-                    {
-                        Console.WriteLine((i + 1) + ". " + toDoList[i]);
-                    }
-                    break;
-
-                case "exit":
-                    break;
-
-                default:
-                    Console.WriteLine("Invalid command.");
-                    break;
-            }
-        }
     }
 }
-```
